e89c3e7179844f6decbbf57bb6ae91cec3188625
prafulpawar <prafuldukhi@gmail.com>
2025-02-22 22:14:32 +0530

    __Done

---
+++ frontend/src/context/userConext.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { createContext } from 'react'
+
+export const MyContext = createContext("");
+
+function userConext() {
+  return (
+    <div></div>
+  )
+}
+
+export default userConext
