2ab513ca0b05233e87536fc4586be4bbcfa848de
prafulpawar <prafuldukhi@gmail.com>
2025-02-22 23:48:59 +0530

    __Category_Controller

---
+++ Backend/src/controller/categoryController.js
@@ -0,0 +1,10 @@
+module.exports.categoryController = async(req,res)=>{
+    try{
+
+    }
+    catch(error){
+        return res.status(400).json({
+            message:"Failed To  Create Category"
+        })
+    }
+}
