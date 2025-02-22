7f2260adda77252203af658084a30abd33b86a83
prafulpawar <prafuldukhi@gmail.com>
2025-02-22 19:34:01 +0530

    __Done

--- Backend/src/controller/userController.js
+++ Backend/src/controller/userController.js
@@ -122,3 +122,14 @@ module.exports.profileController = async(req,res)=>{
     })
   }
 }
+
+module.exports.updateProfile = async(req,res)=>{
+    try{
+
+    }
+    catch(error){
+      return res.status(400).json({
+        message: error.message
+      })
+    }
+}
