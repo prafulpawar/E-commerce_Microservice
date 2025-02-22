72c32512bf29c24336447e8ade4a379c432b68e2
prafulpawar <prafuldukhi@gmail.com>
2025-02-22 22:18:45 +0530

    category_Controller

---
+++ Backend/src/models/categoryModel.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const categoryController = new mongoose.Schema({
+
+})
+
+const categoryModel = mongoose.model('category',categoryController)
+module.exports = categoryModel
