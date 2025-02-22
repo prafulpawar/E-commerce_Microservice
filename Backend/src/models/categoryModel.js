b5fc44dca63ed12200ce4bca7819112ca97603a5
prafulpawar <prafuldukhi@gmail.com>
2025-02-22 22:30:44 +0530

    __Category_Added

--- Backend/src/models/categoryModel.js
+++ Backend/src/models/categoryModel.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose");
 const categoryController = new mongoose.Schema({
+        category:{
+            type:String,
+            require:true
+        },
+        image:{
+            type:String,
+            default:'https://cdn2.iconfinder.com/data/icons/graphic-design-41/48/21-photo-512.png'
+        },
+        subcategory:{
 
+        }
 })
 
 const categoryModel = mongoose.model('category',categoryController)
