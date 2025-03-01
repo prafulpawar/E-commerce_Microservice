1f676911f344fbc2e90b6e3af3c0469e5b25c9a6
prafulpawar <prafuldukhi@gmail.com>
2025-03-02 00:45:03 +0530

    __Done

--- Backend/admin/controllers/categoryController.js
+++ Backend/admin/controllers/categoryController.js
@@ -1,8 +1,11 @@
-module.exports.categoryController = (req,res)=>{
+const categoryModel = require("../models/categorymodel");
+
+module.exports.categoryController =async (req,res)=>{
        try{
 
-           const {name,subcategory} = req.body;
-           if(!name){
+           const {category,subcategory} = req.body;
+
+           if(!category){
             return res.status(400).json({
                 message:"Category Required"
             })
@@ -13,8 +16,37 @@ module.exports.categoryController = (req,res)=>{
                 message:"Category Required"
             })
            }
+            
+          //cheking category or subcategory exists
+          const categoryExists = await categoryModel.findOne({
+            category
+          })
 
+          if(categoryExists){
+              return res.status(400).json({
+                message:"Category Is Exists"
+              })
+          }
 
+          const subcategoryExists = await categoryModel.findOne({
+              subcategory 
+          })
+
+          if(subcategoryExists){
+            return res.status(400).json({
+                message:"Subcategory Is Exists"
+              })
+          }
+
+          //Making An Category 
+          const createdCategory = await categoryModel.create({
+                category,
+                subcategory
+          })
+         return res.status(200).json({
+            data:createdCategory,
+            message:true
+         })
        }
        
        catch(error){
--- Backend/admin/models/categorymodel.js
+++ Backend/admin/models/categorymodel.js
@@ -1,19 +1,22 @@
 const mongoose = require('mongoose');
 
-const categorySchema =new mongoose.Schema({
-           name:{
-              type:String,
-              required:true,
-              minlength:[3,'Minimum minlength Is 3'],
-              maxlength:[50,'Minimum minlength Is 50']
-           },
-           image:{
-              type:String
-           },
-           subcategory:{
-              type:String,
-           },
+const categorySchema = new mongoose.Schema({
+   category: {
+      type: String,
+      required: true,
+      minlength: [3, 'Minimum minlength Is 3'],
+      maxlength: [50, 'Minimum minlength Is 50']
+   },
+   image: {
+      type: String
+   },
+   subcategory: [
+      {
+         type: String,
+         default:undefined
+      }
+   ],
 })
 
-const categoryModel = mongoose.model('category',categorySchema);
+const categoryModel = mongoose.model('category', categorySchema);
 module.exports = categoryModel
