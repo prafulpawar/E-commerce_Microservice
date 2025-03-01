d82a755f4ec5014867f515a06df302049b50e919
prafulpawar <prafuldukhi@gmail.com>
2025-03-01 18:04:49 +0530

    __Done

--- Backend/admin/index.js
+++ Backend/admin/index.js
@@ -7,9 +7,9 @@ const PORT = 5002;
 const connectDB = require('./services/db')
 connectDB();
 
-// app.get("/", (req, res) => {
-//   res.send("Admin Service is Running...");
-// });
+app.get("/", (req, res) => {
+  res.send("Admin Service is Running...");
+});
 
 app.listen(PORT, () => {
   console.log(`Admin Service running on http://localhost:${PORT}`);
