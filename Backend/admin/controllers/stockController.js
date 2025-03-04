d145ed28ca0f555e85b386a429e9d5f932aa7507
prafulpawar <prafuldukhi@gmail.com>
2025-03-04 14:28:18 +0530

    __Admin_Done

--- Backend/admin/controllers/stockController.js
+++ Backend/admin/controllers/stockController.js
@@ -28,7 +28,7 @@ module.exports.stockDetailsCategory = async(req,res)=>{
          
         
         return res.status(200).json({
-             data:existsCat,
+             data:products,
              message:"Successfully Fetched Category"
         })
 
@@ -45,8 +45,52 @@ module.exports.stockDetailsCategory = async(req,res)=>{
 module.exports.stockDetailsProduct = async(req,res)=>{
     try{
          const {product} = req.body;
+         if(!product){
+            return res.status(500).json({
+                message:"Product Is Required"
+            })
+         }
 
+        //  now match product based on theire category && subcategory;
+        //   const products = await categoryModel.find(product)
+        // now we can solve this problem using pipline agrregation
+
+        db.collection1.aggregate([
+
+            {
+          
+              "$lookup": {
+          
+                "from": "collection2", // Target collection to join
+          
+                "localField": "commonField", // Field in the current document to match
+          
+                "foreignField": "commonField", // Matching field in the target collection
+          
+                "as": "joinedData" // Alias for the joined documents
+          
+              }
+          
+            },
+          
+            {
+          
+              "$match": {
+          
+                "joinedData.someField": "desiredValue" // Filter based on the joined data
+          
+              }
+          
+            }
+          
+          ]) 
+          
+   
     }
+
+
+
+    
     catch(error){
         return res.status(400).json({
             message:"Cant Get Details Of Stock"
